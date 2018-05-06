43287000d71da9d3977032c9e0db12049ed83f53
Chuancong Gao <chuanconggao@users.noreply.github.com>
2018-05-06 11:35:26 -0700

    Add docstring to misctools

--- extratools/misctools.py
+++ extratools/misctools.py
@@ -3,10 +3,33 @@
 from typing import *
 
 def cmp(a: Any, b: Any) -> int:
+    """
+    Restores the useful `cmp` function previously in Python 2.
+
+    - Implemented according to [What's New in Python 3.0](https://docs.python.org/3.0/whatsnew/3.0.html#ordering-comparisons).
+
+    Args:
+        a: An object.
+        b: An object.
+
+    Returns:
+        An integer of -1, 0, or 1, denoteing whether a > b, a == b, or a < b, respectively.
+    """
+
     return (a > b) - (a < b)
 
 
 def parsebool(s: str) -> bool:
+    """
+    Parses a string to boolean.
+
+    Args:
+        s: A string.
+
+    Returns:
+        Whether the string contains certain keywords denoting positive.
+    """
+
     return s.strip().lower() in {
         "1", "+",
         "true", "yes", "positive",
